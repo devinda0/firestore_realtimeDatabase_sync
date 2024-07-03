096c430cd0352716c23709a10bbc2e1ddda2f3d1
Devinda Dilshan <devindadilshan0@gmail.com>
2024-07-03 19:59:52 +0530

    change path of accountKey

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./accountKey.json');
+const serviceAccount = require('./accountKey/accountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
